7ea90811b2ff48a84f2f3c55d6205d7ae2648e7a
huxiaozhong <huxiaozhong6030@huxiaozhongdeMacBook-Pro-2.local>
2017-10-05 17:24:08 +0800

    -

--- src/components/flatlist/index.js
+++ src/components/flatlist/index.js
@@ -1,7 +1,115 @@
 import {FlatList} from 'react-native';
+import React from 'react'
+import { View,PanResponder,UIManager,LayoutAnimation } from 'react-native'
 /*
 https://github.com/shiwenwen/react-native-swRefresh
 https://github.com/react-native-component/react-native-smart-pull-to-refresh-listview
 https://github.com/greatbsky/react-native-pull
 */
-export default FlatList;
+
+const isVerticalGesture = (x, y) => {
+    return (Math.abs(x) < Math.abs(y));
+};
+
+const isDownGesture = (x, y) => {
+    return y > 0 && (y > Math.abs(x));
+};
+const isUpGesture = (x, y) => {
+    return y < 0 && (Math.abs(x) < Math.abs(y));
+};
+
+export default class FL extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.state = {
+			offset:0
+		};
+		this.horizontal = false;
+		this.scrollValue = 0;
+		this.startPos = {dx:0,dy:0};
+		this.isDown = false;
+		this._panResponder = PanResponder.create({
+	      onStartShouldSetPanResponder: this.onShouldSetPanResponder.bind(this),
+	      onMoveShouldSetPanResponder: this.onShouldSetPanResponder.bind(this),
+	      onStartShouldSetPanResponderCapture:this.onStartShouldSetPanResponderCapture.bind(this),
+	      onMoveShouldSetPanResponderCapture: this.onMoveShouldSetPanResponderCapture.bind(this),
+	      onPanResponderTerminationRequest: (event, gestureState) => true,
+	      onPanResponderGrant: this.onTouchStart.bind(this),
+	      onPanResponderMove: this.onTouchMove.bind(this),
+	      onPanResponderRelease: this.onTouchEnd.bind(this)
+	    })
+	}
+	/*
+		flat的自带滚动，能滚 则不能得到move
+
+		flat不能滚 则可以得到move
+		
+		onStartShouldSetPanResponderCapture 设置为true的时候
+		表示接受触摸相应 则不会响应具体的点击事件
+	
+		onStartShouldSetPanResponderCapture false onMoveShouldSetPanResponderCapture true 不能滚动 但可以响应点击
+
+
+		onStartShouldSetPanResponderCapture onMoveShouldSetPanResponderCapture
+		都设成false 才能滚
+
+		onStartShouldSetPanResponder onMoveShouldSetPanResponder 
+		false 能滚 true 不能滚
+
+
+
+	*/
+
+	onScroll(e){
+		var ne = e.nativeEvent;
+		this.scrollValue = this.horizontal?ne.contentOffset.x:ne.contentOffset.y;
+	}
+
+	onStartShouldSetPanResponderCapture(e,gestureState){
+		return false;
+	}
+
+	onMoveShouldSetPanResponderCapture(e,gestureState){
+		return false;
+	}
+
+	onShouldSetPanResponder(e,gestureState){
+	    if(gestureState.dx<gestureState.dy&&this.scrollValue<4){
+	    	//向下 在顶端端
+	    	return true;
+	    }
+      	return false;
+	}
+
+	onTouchStart(e,gestureState){
+		
+		this.startPos = {pageX:e.nativeEvent.pageX,pageY:e.nativeEvent.pageY};
+	}
+
+	onTouchMove(e,gestureState){
+		var diff = this.horizontal?e.nativeEvent.pageX-this.startPos.pageX:e.nativeEvent.pageY-this.startPos.pageY;
+		console.log("move:"+diff);
+		this.setState({offset:diff/3});
+	}
+
+	onTouchEnd(){
+		UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+	    LayoutAnimation.easeInEaseOut();
+		this.setState({offset:0});
+	}
+
+	render(){
+		return <View 
+			{...this._panResponder.panHandlers} 
+			style={{flex:1,overflow:"hidden"}}>
+				<View style={{height:this.state.offset,backgroundColor:"red"}}/>
+				<View style={{height:100,backgroundColor:"red",marginTop:-100}}/>
+				<FlatList 
+				 onScroll = {this.onScroll.bind(this)}
+				 bounces={false} 
+				 {...this.props}/>
+		</View>
+	}
+}
+
