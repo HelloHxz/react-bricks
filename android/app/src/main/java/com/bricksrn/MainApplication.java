362417f9c7448f71dde8917d2ce6da11ddb65a33
huxiaozhong <huxiaozhong6030@huxiaozhongdeMacBook-Pro-2.local>
2017-10-08 16:16:05 +0800

    -

--- android/app/src/main/java/com/bricksrn/MainApplication.java
+++ android/app/src/main/java/com/bricksrn/MainApplication.java
@@ -3,6 +3,7 @@ package com.bricksrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.wix.interactable.Interactable;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LottiePackage(),
             new Interactable(),
             new SvgPackage()
       );
