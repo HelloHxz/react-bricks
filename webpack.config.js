e407e4dcfd9c7683768b5a8e8307a51f35d3b12d
huxiaozhong <huxiaozhong6030@huxiaozhongdeMacBook-Pro-2.local>
2017-10-13 22:23:48 +0800

    -

--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,6 @@ function getEntryAndHtmlPlugin(){
 
 module.exports = function (env) {
 
-  //写入less  可以在这里修改主题 以及适配系数 默认是基于iphone6
-  //writeCommonless();
- 
-
   const nodeEnv = env && env.prod ? 'production' : 'development';
   const isProd = nodeEnv === 'production';
   var entryAndHtmlPlugin = getEntryAndHtmlPlugin();
@@ -204,64 +200,3 @@ return {
 }
 
 
-
-function mkdirs(dirname, callback) {
-    fs.exists(dirname, function (exists) {
-        if (exists) {
-            callback();
-        } else {
-            //console.log(path.dirname(dirname));
-            mkdirs(path.dirname(dirname), function () {
-                fs.mkdir(dirname, callback);
-            });
-        }
-    });
-}
-
-
-
-function writeCommonless(){
-   const rem = 75;
-  /*
-    这里可以配置框架的适配参数 iphone 6 为 75 iphone6p 为124.2 iphone为64
-    rem 计算方法
-    var docEl = document.documentElement;
-    var dpr = window.devicePixelRatio || 1;
-    console.log(docEl.clientWidth * dpr / 10) ;
-  */
-  mkdirs(path.join(__dirname, '/node_modules/xz-lightapp/css'),function(){
-    fs.writeFile(path.join(__dirname, '/node_modules/xz-lightapp/css/common.less'), 
-        `@textcolor:#333;
-         @headerbordercolor:rgb(171,171,173);
-         @backgroundcolor:#fff;
-         @themecolor:rgb(10,96,254);
-         @bordercolor:rgb(194,192,198);
-         .px2rem(@name, @px){ @{name}: @px / ${rem} * 1rem;}
-         .px2px(@name, @px){
-            @{name}: round(@px / 2) * 1px;
-            [data-dpr="2"] & {
-                @{name}: @px * 1px;
-            }
-            [data-dpr="2.5"] & {
-                @{name}: round(@px * 2.5 / 2) * 1px;
-            }
-            [data-dpr="2.75"] & {
-                @{name}: round(@px * 2.75 / 2) * 1px;
-            }
-            [data-dpr="3"] & {
-                @{name}: round(@px / 2 * 3) * 1px
-            }
-            [data-dpr="4"] & {
-                @{name}: @px * 2px;
-            }
-        }
-         .px2remtransfrom(@x,@y){
-            transform: translate3d( @x / ${rem} * 1rem,  @y / ${rem} * 1rem, 0);
-            -webkit-transform: translate3d( @x / ${rem} * 1rem,  @y / ${rem} * 1rem, 0);
-         }`,
-         function (err) {
-          if (err) throw err;
-          console.log("common.less write success!!");
-      });
-  });
-}
