c7582565e6734f18d2b29d65fe66e7fcc44d5e26
huxiaozhong <huxiaozhong6030@huxiaozhongdeMacBook-Pro-2.local>
2017-10-28 15:46:36 +0800

    -

--- src/components/picker/index.ios.js
+++ src/components/picker/index.ios.js
@@ -1,6 +1,7 @@
 import {Picker} from 'react-native'
 import React from 'react';
 import View from '../view'
+import Base from './common'
 import StyleSheet from '../style'
 
 function px(val){
@@ -39,7 +40,7 @@ class SelectorColumn extends React.Component{
 
 
 
-export default class P extends React.Component{
+export default class P extends Base{
 
 	constructor(props){
 		super(props);
@@ -52,7 +53,7 @@ export default class P extends React.Component{
 	        this.cascadeCount = parseInt(this.props.cascadeCount);
 	      }
 	    }
-
+	    this.SelectorColumn = SelectorColumn;
 	    this.columnsCount = this.cascadeCount||this.props.datasource.length;
         this.selectedIndexs = this.getSelectedIndexs(props);
         this.instanceDict={};
@@ -61,147 +62,6 @@ export default class P extends React.Component{
 		}
 	}
 
-	 getSelectedIndexs(props){
-	    var re = [];
-	    if(props.selectedValues){
-	      if(this.isCascade){
-	        return this.getSelecedIndexsByValueWhenIsCascade(props);
-	      }else{
-	        return this.getSelectedIndexsByValue(props);
-	      }
-	    }else if(props.selectedIndexs){
-	      if(!(props.selectedIndexs instanceof Array)){
-	         return this.getDefaultSelectedIndexs();
-	      }
-	      return this.getDefaultSelectedIndexs(props.selectedIndexs);
-	    }else{
-	      return this.getDefaultSelectedIndexs();
-	    }
-	    return this.getDefaultSelectedIndexs();
-	  }
-
-	  getSelectedIndexsByValue(props){
-	    var value = props.selectedValues||[];
-	    if(!(value instanceof Array)){
-	      value = [];
-	    }
-	    var selectedIndexs = [];
-	    var index = 0;
-	    for(var i=0;i<this.columnsCount;i++){
-	      index = this._getIndexByValue(value[i],props.datasource[i]);
-	      selectedIndexs.push(index);
-	    }
-	    return selectedIndexs;
-	  }
-
-	  getSelecedIndexsByValueWhenIsCascade(props){
-	    var value = props.selectedValues||[];
-	    if(!(value instanceof Array)){
-	      value = [];
-	    }
-	    var curData = props.datasource[0];
-	    var selectedIndexs = [];
-	    var index = 0;
-	    for(var i=0;i<this.columnsCount;i++){
-	      if(i===0){
-	        index = this._getIndexByValue(value[i],curData);
-	        curData = curData[index].children||[];
-	      }else{
-	        index = this._getIndexByValue(value[i],curData||[]);
-	        var c = curData[index]||{};
-	        curData = c.children||[];
-	      }
-	      selectedIndexs.push(index);
-	    }
-
-	    return selectedIndexs;
-
-	  }
-
-
-  _getIndexByValue(value,arr){
-    if(!value){
-      return 0;
-    }
-    arr = arr||[];
-    var index = 0;
-    for(var i=0,j=arr.length;i<j;i++){
-      if(arr[i].label===value){
-        index = i;
-        break;
-      }
-      if(this.props.valueIsInt){
-        if(parseInt(arr[i].label)===parseInt(value)){
-          index = i;
-          break;
-        }
-      }
-    }
-    return index;
-  }
-
-  getDefaultSelectedIndexs(paramsArr){
-    var re=[];
-    paramsArr = paramsArr||[];
-    for(var i=0;i<this.columnsCount;i++){
-      var p = paramsArr[i]||0;
-      if(isNaN(p)){
-        p=0;
-      }else{
-        p=parseInt(p);
-      }
-      re.push(p);
-    }
-    return re;
-  }
-
-
-  repaireIndex(index,columData){
-  
-    if(index<0||index>=columData.length){
-      return 0;
-    }
-    return index;
-
-  }
-
-
-
-  getColumns(){
-  	 var columns =[];
-
-    if(this.isCascade){
-      var preSelectedItemData = null;
-      for(var i=0;i<this.columnsCount;i++){
-        var curkey = "column_"+i;
-        var data = [];
-        var selectedIndexInCol = this.selectedIndexs[i];
-        if(i===0){
-          data = this.props.datasource[0];
-          selectedIndexInCol = this.repaireIndex(selectedIndexInCol,data);
-          preSelectedItemData = data[selectedIndexInCol].children||[];
-        }else{
-          data = preSelectedItemData;
-          selectedIndexInCol = this.repaireIndex(selectedIndexInCol,data);
-          if(!preSelectedItemData[selectedIndexInCol]){
-            preSelectedItemData = [];
-          }else{
-           preSelectedItemData = preSelectedItemData[selectedIndexInCol].children||[];
-          }
-        }
-        columns.push(<SelectorColumn selectedIndex={selectedIndexInCol} columnIndex={i} data={data} parent={this} pkey={curkey} itemHeight={this.itemHeight} key={curkey}/>);
-      }
-    }else{
-      for(var i=0;i<this.columnsCount;i++){
-        var curkey = "column_"+i;
-        var data = this.props.datasource[i];
-         var selectedIndexInCol = this.selectedIndexs[i];
-        columns.push(<SelectorColumn columnIndex={i} selectedIndex={selectedIndexInCol} data={data} parent={this} pkey={curkey} itemHeight={this.itemHeight} key={curkey}/>);
-      }
-    }
-    return columns;
-  }
- 
 
 	render(){
 		return (
--- src/components/picker/index.web.js
+++ src/components/picker/index.web.js
@@ -3,6 +3,8 @@ import StyleSheet from "../style"
 import View from '../view'
 import Text from '../text'
 
+import Base from './common'
+
 
 function getPageX(e){
 	if(StyleSheet.isWeb){
@@ -273,11 +275,11 @@ function px(val){
 }
 
 //cascade
-class Selector extends React.Component {
+class Selector extends Base {
   constructor(props) {
     super(props)
     this.hasInit = false;
-
+    this.SelectorColumn = SelectorColumn;
     this.itemHeight = StyleSheet.px2px(75);
     this.instanceDict = {};
     this.isCascade = false;
@@ -322,109 +324,6 @@ class Selector extends React.Component {
     this.curColumn.onTouchEnd();
   }
 
-
-  getSelectedIndexs(props){
-    //this.columnsCount
-    var re = [];
-   
-    if(props.selectedValues){
-      if(this.isCascade){
-        return this.getSelecedIndexsByValueWhenIsCascade(props);
-      }else{
-        return this.getSelectedIndexsByValue(props);
-      }
-    }else if(props.selectedIndexs){
-      if(!(props.selectedIndexs instanceof Array)){
-         return this.getDefaultSelectedIndexs();
-      }
-      return this.getDefaultSelectedIndexs(props.selectedIndexs);
-    }else{
-      return this.getDefaultSelectedIndexs();
-    }
-    return this.getDefaultSelectedIndexs();
-  }
-
-  getSelectedIndexsByValue(props){
-    var value = props.selectedValues||[];
-    if(!(value instanceof Array)){
-      value = [];
-    }
-    var selectedIndexs = [];
-    var index = 0;
-    for(var i=0;i<this.columnsCount;i++){
-      index = this._getIndexByValue(value[i],props.datasource[i]);
-      selectedIndexs.push(index);
-    }
-    return selectedIndexs;
-  }
-
-  getSelecedIndexsByValueWhenIsCascade(props){
-    var value = props.selectedValues||[];
-    if(!(value instanceof Array)){
-      value = [];
-    }
-    var curData = props.datasource[0];
-    var selectedIndexs = [];
-    var index = 0;
-    for(var i=0;i<this.columnsCount;i++){
-      if(i===0){
-        index = this._getIndexByValue(value[i],curData);
-        curData = curData[index].children||[];
-      }else{
-        index = this._getIndexByValue(value[i],curData||[]);
-        var c = curData[index]||{};
-        curData = c.children||[];
-      }
-      selectedIndexs.push(index);
-    }
-
-    return selectedIndexs;
-
-  }
-
-  _getIndexByValue(value,arr){
-    if(!value){
-      return 0;
-    }
-    arr = arr||[];
-    var index = 0;
-    for(var i=0,j=arr.length;i<j;i++){
-      if(arr[i].label===value){
-        index = i;
-        break;
-      }
-      if(this.props.valueIsInt){
-        if(parseInt(arr[i].label)===parseInt(value)){
-          index = i;
-          break;
-        }
-      }
-    }
-    return index;
-  }
-
-  getDefaultSelectedIndexs(paramsArr){
-    var re=[];
-    paramsArr = paramsArr||[];
-    for(var i=0;i<this.columnsCount;i++){
-      var p = paramsArr[i]||0;
-      if(isNaN(p)){
-        p=0;
-      }else{
-        p=parseInt(p);
-      }
-      re.push(p);
-    }
-    return re;
-  }
-
-  bkClick(){
-    this.props.onBackLayerClick&&this.props.onBackLayerClick();
-  }
-
-
-
-
   renderMidArea(){
     if(this.props.renderMidArea){
       return this.props.renderMidArea();
@@ -433,56 +332,7 @@ class Selector extends React.Component {
   }
 
 
-
-  repaireIndex(index,columData){
-  
-    if(index<0||index>=columData.length){
-      return 0;
-    }
-    return index;
-
-  }
-
-
-  getColumns(){
-  	 var columns =[];
-
-    if(this.isCascade){
-      var preSelectedItemData = null;
-      for(var i=0;i<this.columnsCount;i++){
-        var curkey = "column_"+i;
-        var data = [];
-        var selectedIndexInCol = this.selectedIndexs[i];
-        if(i===0){
-          data = this.props.datasource[0];
-          selectedIndexInCol = this.repaireIndex(selectedIndexInCol,data);
-          preSelectedItemData = data[selectedIndexInCol].children||[];
-        }else{
-          data = preSelectedItemData;
-          selectedIndexInCol = this.repaireIndex(selectedIndexInCol,data);
-          if(!preSelectedItemData[selectedIndexInCol]){
-            preSelectedItemData = [];
-          }else{
-           preSelectedItemData = preSelectedItemData[selectedIndexInCol].children||[];
-          }
-        }
-        columns.push(<SelectorColumn selectedIndex={selectedIndexInCol} columnIndex={i} data={data} parent={this} pkey={curkey} itemHeight={this.itemHeight} key={curkey}/>);
-      }
-    }else{
-      for(var i=0;i<this.columnsCount;i++){
-        var curkey = "column_"+i;
-        var data = this.props.datasource[i];
-         var selectedIndexInCol = this.selectedIndexs[i];
-        columns.push(<SelectorColumn columnIndex={i} selectedIndex={selectedIndexInCol} data={data} parent={this} pkey={curkey} itemHeight={this.itemHeight} key={curkey}/>);
-      }
-    }
-    return columns;
-  }
- 
-
-
   render() {
-   
 
     return (
         <View 
