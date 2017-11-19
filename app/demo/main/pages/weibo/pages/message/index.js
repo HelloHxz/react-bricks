e5c503eadcd68a8a1ec97d9898969d179323f285
huxiaozhong <huxiaozhong6030@huxiaozhongdeMacBook-Pro-2.local>
2017-11-19 11:38:03 +0800

    -

--- app/demo/main/pages/weibo/pages/message/index.js
+++ app/demo/main/pages/weibo/pages/message/index.js
@@ -9,6 +9,12 @@ class MessagePage extends React.Component {
     return (
       <View style={{flex:1,backgroundColor:Theme.theme_background_color}}>
          <IOSHeaderPlaceHolder/>
+         <View style={StyleSheet.create({height:80,backgroundColor:"#fff",justifyContent:"center",alignItems:"center"})}>
+            <Button type='primary' style={StyleSheet.create({backgroundColor:"#eee",height:60,width:700})}>
+              <Icon style={StyleSheet.create({fontSize:30,color:"gray"})} icon={Icon.DemoIcons.search}/>
+              <Text style={StyleSheet.create({fontSize:26,color:"gray",marginLeft:10})}>搜索</Text>
+            </Button>
+         </View>
       </View>
     );
   }
